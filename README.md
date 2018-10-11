305433c1348b7c656731ad2e87ee393445848d49
Ярослав Кикоть <zicrael@MacBook-Pro-Aroslav.local>
2018-10-11 20:21:01 +0300

    init commit

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# vue-random-users
-Vue.js app with Random users API
+# random-users-vue
+
+## Project setup
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+```
+npm run build
+```
+
+### Lints and fixes files
+```
+npm run lint
+```
---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <h1>{{title}}</h1>
+    <TableView></TableView>
+  </div>
+</template>
+
+<script>
+import TableView from './components/table-view.vue'
+
+export default {
+  name: 'app',
+  components: {
+    TableView
+  },
+   data: function () {
+    return {
+      title: 'Random users vue app'
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+.d-flex {
+  display: flex;
+} 
+.h-100 {
+  height: 100%;
+} 
+.w-100 {
+  width: 100%;
+}
+</style>
